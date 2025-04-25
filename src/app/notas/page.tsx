7bbe0e791e40dacf50d3c7acd5618f15b19e4e06
Luis Reyes <alfredluis177@gmail.com>
2025-04-25 12:49:28 -0600

    mejoré el diseño del componente Notas agregando una tarjeta con fondo y filtro de desenfoque

--- src/app/notas/page.tsx
+++ src/app/notas/page.tsx
@@ -1,8 +1,21 @@
 export default function Notas() {
   return (
-    <div className="d-flex flex-column align-items-center justify-content-center vh-100 text-white">
+    <div
+      className="card d-flex flex-column align-items-center justify-content-center vh-100 text-white"
+      style={{
+        backgroundColor: "rgba(0,0,0,0.55)",
+        backdropFilter: "blur(60px)",
+      }}
+    >
       <h1 className="text-center">Notas</h1>
       <p className="lead text-center">Aquí podrás ver tus notas.</p>
+      <p>llassmalsmckamslacs</p>
+      <b>laskkalsklansnas</b>
+      <ul>
+        <li>lkassd</li>
+        <li>klasldm</li>
+        <li>laslk</li>
+      </ul>
     </div>
   );
 }
